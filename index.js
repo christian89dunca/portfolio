7fa31169d87344766ff74a995f60148023767d02
Christian D. Dunca <christiand.dunca@Christians-MacBook-Pro.local>
2020-06-24 15:31:11 +0200

    first commit project

---
+++ index.js
@@ -0,0 +1,20 @@
+let i = 0;
+
+for (i; i <= 100; i++) {
+  if (i % 3 == 0 && i % 5 == 0) {
+    console.log("mr.robot");
+  } else if {
+    if (i % 3 == 0) {
+      console.log("Mr");
+    }
+    else (i % 5 == 0) {
+      console.log("Robot");
+    }
+  } else {
+    (i % 3 != 0 && i % 5 != 0) {
+        console.log(i);
+      }
+  }
+
+
+}
